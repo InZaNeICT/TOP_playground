0bb4bee7b4dc332e6f20f3f8248f51d8e60d3cec
Temeeeeeee <teemu.kuhanen@edu.ekky.fi>
2021-10-01 10:09:51 +0300

    new init

--- MaterialTrackingTestBench.py
+++ MaterialTrackingTestBench.py
@@ -1,6 +1,6 @@
 import random
 import sys
-
+import keyboard
 
 # DONT TOUCH #
 def saveToDatabase(input):
@@ -12,13 +12,65 @@ def getTag():
 # DONT TOUCH #
 
 # YOUR CODE HERE #
+def rfidtest():
+    run = True
+    while run:
+        try:
+            print("press enter to ask If rfid was found")
+            if keyboard.is_pressed("Enter"):
+                id = getTag()
+                print(id)
+            if id:
+                print("rfid Tag Was Found")
+                saveToDatabase()
+                print("Saving to database")
+                return True
+            else:
+                print("rfid tag not found")
+                pass
+        except:
+            pass
+'''
+def Questions():
+    run = True
+    yes = {'y'}
+    no = {'n'}
+    while run:
+        try:
+            print("Did the test succeed [y/n]?")
+            choice = input().lower()
+            if choice in yes:
+                print("are you sure [y/n]?")
+                if choice in yes:
+                    return True
+                elif choice in no:
+                    return
+            elif choice in no:
+                return True
+            print("Your answer is saved to database")
+            saveToDatabase()
+        except:
+            pass
+'''
+'''
+def Saver():
+    run = True
+'''
 
- cxvdxvdvdsvd
+
+
 
 # MAIN PROGRAM
+run = True
+while run:
+    rfidtest()
+    print("ok")
+    
+    run = False
 
 # MAIN PROGRAM
 # YOUR CODE HERE #
+
 
 '''
 TIPS:
